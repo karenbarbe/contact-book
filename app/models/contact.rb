c67acd459f932187322396369b4503f54b992419
Karen Barbé <info@karenbarbe.com>
2025-08-29 11:41:45 -0500

    Update contact.rb

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: contacts
+#
+#  id               :integer          not null, primary key
+#  city             :text
+#  date_of_birth    :date
+#  first_name       :text
+#  last_name        :text
+#  notes            :text
+#  phone            :text
+#  state            :text
+#  street_address_1 :text
+#  street_address_2 :text
+#  username         :string
+#  zip              :text
+#  created_at       :timestamptz
+#
 class Contact < ActiveRecord::Base
 end
